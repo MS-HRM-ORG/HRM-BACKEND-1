7c5f51f5290a437b6a3c257ae0b94f061dc44f15
HP <HP@Akhil>
2024-06-09 21:46:24 +0530

    Role repo commit

--- src/main/java/com/msmavas/HRMS_Backend/repositories/RoleRepository.java
+++ src/main/java/com/msmavas/HRMS_Backend/repositories/RoleRepository.java
@@ -3,7 +3,7 @@ package com.msmavas.HRMS_Backend.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.msmavas.HRMS_Backend.models.Role;
-
+//repo
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 }
 
